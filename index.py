a598088d3f8615d312599d9750741451486be3ce
mibi88 <76903855+mibi88@users.noreply.github.com>
2021-04-02 22:21:33 +0200

    v.0.2

--- index.py
+++ index.py
@@ -1,10 +1,21 @@
 from tkinter import *
-from tkinter.ttk import *
+# from tkinter import ttk
+# from tkinter.ttk import *
 from math import *
 
-root = Tk()
-root.resizable(width = 0, height = 0)
-root.title("MibiCalc")
+main = Tk()
+# main.resizable(width = 0, height = 0)
+main.title("MibiCalc")
+main.rowconfigure(0, weight=1)
+main.rowconfigure(1, weight=1)
+main.columnconfigure(1, weight=1)
+main.rowconfigure(2, weight=1)
+main.columnconfigure(2, weight=1)
+main.rowconfigure(3, weight=1)
+main.columnconfigure(3, weight=1)
+main.rowconfigure(4, weight=1)
+main.columnconfigure(4, weight=1)
+main.rowconfigure(5, weight=1)
 def parentheseopen():
     #
     print("(")
@@ -57,6 +68,15 @@ def clear():
     #
     print("AC")
     calcs.delete(0, END)
+def delchar():
+    calc_todel = calcs.get()
+    calc = calc_todel[:-1]
+    #---replace the text of the entry---
+    #print(calc)
+    # calcs.config(text=calc)
+    calcs.delete(0, END)
+    calcs.insert(0, calc)
+    #-----------------------------------
 #---
 def div():
     #
@@ -89,65 +109,68 @@ def execute():
         calcs.insert(END, "ERROR")
 
 #entry
-calcs = Entry(root, width = 10)
-calcs.grid(column = 2, row = 1)
+calcs = Entry(main, width = 10)
+calcs.grid(column = 1, row = 0, sticky='nesw', columnspan=4)
 #buttons
 #(
-parentheseopenb = Button(root, text="(", command=parentheseopen)
-parentheseopenb.grid(column=1, row=5)
+parentheseopenb = Button(main, text="(", command=parentheseopen)
+parentheseopenb.grid(column=1, row=5, sticky='nesw')
 #)
-parenthesecloseb = Button(root, text=")", command=parentheseclose)
-parenthesecloseb.grid(column=3, row=5)
+parenthesecloseb = Button(main, text=")", command=parentheseclose)
+parenthesecloseb.grid(column=3, row=5, sticky='nesw')
 #1
-oneb = Button(root, text="1", command=one)
-oneb.grid(column=1, row=2)
+oneb = Button(main, text="1", command=one)
+oneb.grid(column=1, row=2, sticky='nesw')
 #2
-twob = Button(root, text="2", command=two)
-twob.grid(column=2, row=2)
+twob = Button(main, text="2", command=two)
+twob.grid(column=2, row=2, sticky='nesw')
 #3
-threeb = Button(root, text="3", command=three)
-threeb.grid(column=3, row=2)
+threeb = Button(main, text="3", command=three)
+threeb.grid(column=3, row=2, sticky='nesw')
 #4
-fourb = Button(root, text="4", command=four)
-fourb.grid(column=1, row=3)
+fourb = Button(main, text="4", command=four)
+fourb.grid(column=1, row=3, sticky='nesw')
 #5
-fiveb = Button(root, text="5", command=five)
-fiveb.grid(column=2, row=3)
+fiveb = Button(main, text="5", command=five)
+fiveb.grid(column=2, row=3, sticky='nesw')
 #6
-sixb = Button(root, text="6", command=six)
-sixb.grid(column=3, row=3)
+sixb = Button(main, text="6", command=six)
+sixb.grid(column=3, row=3, sticky='nesw')
 #7
-sevenb = Button(root, text="7", command=seven)
-sevenb.grid(column=1, row=4)
+sevenb = Button(main, text="7", command=seven)
+sevenb.grid(column=1, row=4, sticky='nesw')
 #8
-eightb = Button(root, text="8", command=eight)
-eightb.grid(column=2, row=4)
+eightb = Button(main, text="8", command=eight)
+eightb.grid(column=2, row=4, sticky='nesw')
 #9
-nineb = Button(root, text="9", command=nine)
-nineb.grid(column=3, row=4)
+nineb = Button(main, text="9", command=nine)
+nineb.grid(column=3, row=4, sticky='nesw')
 #0
-zerob = Button(root, text="0", command=zero)
-zerob.grid(column=2, row=5)
+zerob = Button(main, text="0", command=zero)
+zerob.grid(column=2, row=5, sticky='nesw')
 #+
-plusb = Button(root, text="+", command=addnum)
-plusb.grid(column=4, row=2)
+plusb = Button(main, text="+", command=addnum)
+plusb.grid(column=4, row=2, sticky='nesw')
 #-
-minusb = Button(root, text="-", command=minu)
-minusb.grid(column=4, row=3)
+minusb = Button(main, text="-", command=minu)
+minusb.grid(column=4, row=3, sticky='nesw')
 #*
-multiplyb = Button(root, text="*", command=multi)
-multiplyb.grid(column=4, row=4)
+multiplyb = Button(main, text="*", command=multi)
+multiplyb.grid(column=4, row=4, sticky='nesw')
 #/
-divb = Button(root, text="/", command=div)
-divb.grid(column=4, row=5)
+divb = Button(main, text="/", command=div)
+divb.grid(column=4, row=5, sticky='nesw')
 #%
-modb = Button(root, text="mod", command=mod)
-modb.grid(column=4, row=1)
+modb = Button(main, text="mod", command=mod)
+modb.grid(column=4, row=1, sticky='nesw')
+#DEL
+clearb = Button(main, text="DEL", background = "#fadd02", activebackground = "#f7e979", command=delchar)
+clearb.grid(column=3, row=1, sticky='nesw')
 #AC
-clearb = Button(root, text="AC", command=clear)
-clearb.grid(column=3, row=1)
+clearb = Button(main, text="AC", background = "#fadd02", activebackground = "#f7e979", command=clear)
+clearb.grid(column=2, row=1, sticky='nesw')
 #EXE button
-clearb = Button(root, text="EXE", command=execute)
-clearb.grid(column=1, row=1)
+clearb = Button(main, text="EXE", background = "#fadd02", activebackground = "#f7e979", command=execute)
+clearb.grid(column=1, row=1, sticky='nesw')
 
-root.mainloop()
+main.mainloop()
