2f5ac98e7fd24236f1415202613ee8923450facc
mibi88 <76903855+mibi88@users.noreply.github.com>
2021-04-03 12:50:52 +0200

    v.0.2.5

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.font import *
 # from tkinter import ttk
 # from tkinter.ttk import *
 from math import *
@@ -107,70 +108,102 @@ def execute():
     except:
         calcs.delete(0, END)
         calcs.insert(END, "ERROR")
+#===
+def refreshsize(event):
+    myFont = Font(weight='bold')
+    oneb['font'] = myFont
+    twob['font'] = myFont
+    threeb['font'] = myFont
+    fourb['font'] = myFont
+    fiveb['font'] = myFont
+    sixb['font'] = myFont
+    sevenb['font'] = myFont
+    eightb['font'] = myFont
+    nineb['font'] = myFont
+    zerob['font'] = myFont
+    #---
+    parentheseopenb['font'] = myFont
+    parenthesecloseb['font'] = myFont
+    #---
+    modb['font'] = myFont
+    plusb['font'] = myFont
+    minusb['font'] = myFont
+    multiplyb['font'] = myFont
+    divb['font'] = myFont
+    #---
+    clearb['font'] = myFont
+    delb['font'] = myFont
+    exeb['font'] = myFont
+    #---
+    calcs['font'] = myFont
 
 #entry
-calcs = Entry(main, width = 10)
+calcs = Entry(main, background = "#405925", foreground = "#34382f", width = 10)
 calcs.grid(column = 1, row = 0, sticky='nesw', columnspan=4)
+#---
+main.configure(bg='#2d5400')
 #buttons
 #(
-parentheseopenb = Button(main, text="(", command=parentheseopen)
+parentheseopenb = Button(main, text="(", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=parentheseopen)
 parentheseopenb.grid(column=1, row=5, sticky='nesw')
 #)
-parenthesecloseb = Button(main, text=")", command=parentheseclose)
+parenthesecloseb = Button(main, text=")", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=parentheseclose)
 parenthesecloseb.grid(column=3, row=5, sticky='nesw')
 #1
-oneb = Button(main, text="1", command=one)
+oneb = Button(main, text="1", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=one)
 oneb.grid(column=1, row=2, sticky='nesw')
 #2
-twob = Button(main, text="2", command=two)
+twob = Button(main, text="2", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=two)
 twob.grid(column=2, row=2, sticky='nesw')
 #3
-threeb = Button(main, text="3", command=three)
+threeb = Button(main, text="3", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=three)
 threeb.grid(column=3, row=2, sticky='nesw')
 #4
-fourb = Button(main, text="4", command=four)
+fourb = Button(main, text="4", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=four)
 fourb.grid(column=1, row=3, sticky='nesw')
 #5
-fiveb = Button(main, text="5", command=five)
+fiveb = Button(main, text="5", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=five)
 fiveb.grid(column=2, row=3, sticky='nesw')
 #6
-sixb = Button(main, text="6", command=six)
+sixb = Button(main, text="6", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=six)
 sixb.grid(column=3, row=3, sticky='nesw')
 #7
-sevenb = Button(main, text="7", command=seven)
+sevenb = Button(main, text="7", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=seven)
 sevenb.grid(column=1, row=4, sticky='nesw')
 #8
-eightb = Button(main, text="8", command=eight)
+eightb = Button(main, text="8", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=eight)
 eightb.grid(column=2, row=4, sticky='nesw')
 #9
-nineb = Button(main, text="9", command=nine)
+nineb = Button(main, text="9", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=nine)
 nineb.grid(column=3, row=4, sticky='nesw')
 #0
-zerob = Button(main, text="0", command=zero)
+zerob = Button(main, text="0", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=zero)
 zerob.grid(column=2, row=5, sticky='nesw')
 #+
-plusb = Button(main, text="+", command=addnum)
+plusb = Button(main, text="+", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=addnum)
 plusb.grid(column=4, row=2, sticky='nesw')
 #-
-minusb = Button(main, text="-", command=minu)
+minusb = Button(main, text="-", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=minu)
 minusb.grid(column=4, row=3, sticky='nesw')
 #*
-multiplyb = Button(main, text="*", command=multi)
+multiplyb = Button(main, text="*", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=multi)
 multiplyb.grid(column=4, row=4, sticky='nesw')
 #/
-divb = Button(main, text="/", command=div)
+divb = Button(main, text="/", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=div)
 divb.grid(column=4, row=5, sticky='nesw')
 #%
-modb = Button(main, text="mod", command=mod)
+modb = Button(main, text="mod", background = "#2e2e2e", activebackground = "#545454", foreground = "#b55e00", activeforeground = "#874600", command=mod)
 modb.grid(column=4, row=1, sticky='nesw')
 #DEL
-clearb = Button(main, text="DEL", background = "#fadd02", activebackground = "#f7e979", command=delchar)
-clearb.grid(column=3, row=1, sticky='nesw')
+delb = Button(main, text="DEL", background = "#fadd02", activebackground = "#f7e979", foreground = "#34382f", activeforeground = "#4b5243", command=delchar)
+delb.grid(column=3, row=1, sticky='nesw')
 #AC
-clearb = Button(main, text="AC", background = "#fadd02", activebackground = "#f7e979", command=clear)
+clearb = Button(main, text="AC", background = "#fadd02", activebackground = "#f7e979", foreground = "#34382f", activeforeground = "#4b5243", command=clear)
 clearb.grid(column=2, row=1, sticky='nesw')
 #EXE button
-clearb = Button(main, text="EXE", background = "#fadd02", activebackground = "#f7e979", command=execute)
-clearb.grid(column=1, row=1, sticky='nesw')
+exeb = Button(main, text="EXE", background = "#fadd02", activebackground = "#f7e979", foreground = "#34382f", activeforeground = "#4b5243", command=execute)
+exeb.grid(column=1, row=1, sticky='nesw')
+#Refresh the font size
+main.bind("<Configure>", refreshsize)
 
 main.mainloop()
